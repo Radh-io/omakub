15b30592a69f964a7cdeb5ed7cb5975210708684
Patrick Macdonald <gh@radh.io>
2025-08-24 14:54:34 +0100

    temporary update of install location. Define install location in env var.

--- boot.sh
+++ boot.sh
@@ -11,20 +11,20 @@ ascii_art='________                  __        ___.
 '
 
 echo -e "$ascii_art"
-echo "=> Omakub is for fresh Ubuntu 24.04+ installations only!"
+echo "=> Popmakub is for fresh Pop-os 22.04+ installations only!"
 echo -e "\nBegin installation (or abort with ctrl+c)..."
 
 sudo apt-get update >/dev/null
 sudo apt-get install -y git >/dev/null
 
-echo "Cloning Omakub..."
-rm -rf ~/.local/share/omakub
-git clone https://github.com/basecamp/omakub.git ~/.local/share/omakub >/dev/null
-if [[ $OMAKUB_REF != "master" ]]; then
-	cd ~/.local/share/omakub
-	git fetch origin "${OMAKUB_REF:-stable}" && git checkout "${OMAKUB_REF:-stable}"
-	cd -
-fi
-
+echo "Cloning Popmakub..."
+rm -rf ~/.local/share/popmakub
+# git clone https://github.com/basecamp/omakub.git ~/.local/share/omakub >/dev/null
+# if [[ $OMAKUB_REF != "master" ]]; then
+# 	cd ~/.local/share/omakub
+# 	git fetch origin "${OMAKUB_REF:-stable}" && git checkout "${OMAKUB_REF:-stable}"
+# 	cd -
+# fi
+POPMAKUB_ROOT=~/src/radhio/omakub
 echo "Installation starting..."
-source ~/.local/share/omakub/install.sh
+source $POPMAKUB_ROOT/install.sh
